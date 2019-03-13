92f95f414399d83a9c3c16f88d789eac3c679a10
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-03-12 21:30:54 -0600

    removes hashes from dist files

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ $angular_project_path = get_stylesheet_directory_uri().'/my-app/dist/my-app/';
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="<?php echo $angular_project_path; ?>favicon.ico">
-<link rel="stylesheet" href="<?php echo $angular_project_path; ?>styles.3ff695c00d717f2d2a11.css"></head>
+<link rel="stylesheet" href="<?php echo $angular_project_path; ?>styles.css"></head>
 <body>
   <app-root></app-root>
-<script type="text/javascript" src="<?php echo $angular_project_path; ?>runtime.26209474bfa8dc87a77c.js"></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>es2015-polyfills.c5dd28b362270c767b34.js" nomodule></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>polyfills.8bbb231b43165d65d357.js"></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>main.5b12663dd35509ec0482.js"></script></body>
+<script type="text/javascript" src="<?php echo $angular_project_path; ?>runtime.js"></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>es2015-polyfills.js" nomodule></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>polyfills.js"></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>main.js"></script></body>
 </html>
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 # wp-angular2-theme
 Wordpress Theme that loads a bundled Angular 2 app
+
+### Notes
+- Build the angular app without hashes
+```sh
+ng build --prod --output-hashing=none
+```

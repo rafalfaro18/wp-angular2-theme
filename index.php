cfbc9e06caa2b85c0610361b1ddc2600d2d39f36
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-03-12 21:06:23 -0600

    loads angular app's production code in wp theme

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-echo "<p>My Angular Theme</p>";
+$angular_project_path = get_stylesheet_directory_uri().'/my-app/dist/my-app/';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title><?php echo get_bloginfo('name'); ?></title>
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="<?php echo $angular_project_path; ?>favicon.ico">
+<link rel="stylesheet" href="<?php echo $angular_project_path; ?>styles.3ff695c00d717f2d2a11.css"></head>
+<body>
+  <app-root></app-root>
+<script type="text/javascript" src="<?php echo $angular_project_path; ?>runtime.26209474bfa8dc87a77c.js"></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>es2015-polyfills.c5dd28b362270c767b34.js" nomodule></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>polyfills.8bbb231b43165d65d357.js"></script><script type="text/javascript" src="<?php echo $angular_project_path; ?>main.5b12663dd35509ec0482.js"></script></body>
+</html>

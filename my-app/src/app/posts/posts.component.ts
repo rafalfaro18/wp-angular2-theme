cbeab4351e4d59da2aca718c4bd1b12b0b241b3b
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-03-13 22:16:41 -0600

    gets posts info into the page

--- my-app/src/app/posts/posts.component.ts
+++ my-app/src/app/posts/posts.component.ts
@@ -8,13 +8,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PostsComponent implements OnInit {
 
+  private posts  = [];
+
   baseUrl = location.href.split('/')[2] === 'localhost:4200' ? 'http://localhost:8081/wp-json/wp/v2' : '/wp-json/wp/v2';
 
   constructor(private httpClient: HttpClient) { }
 
   get_posts(){
-    this.httpClient.get(this.baseUrl + '/posts').subscribe((res)=>{
+    this.httpClient.get(this.baseUrl + '/posts').subscribe((res : any[])=>{
         console.log(res);
+        this.posts = res;
     });
   }
 
